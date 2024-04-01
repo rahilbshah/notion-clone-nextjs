5393d332f03e1bc112e6113e22ab5dad94cc09f9
rahilbshah <98266707+rahilbshah@users.noreply.github.com>
2024-04-01 14:00:40 +0530

    Add Sidebar with Creating,deleting document functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/providers/theme-provider';
 import { ConvexClientProvider } from '@/providers/convex-provider';
+import { Toaster } from 'sonner';
 
 const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
@@ -40,6 +41,7 @@ export default function RootLayout({
             defaultTheme="system"
             storageKey="jotion-theme"
           >
+            <Toaster position="bottom-center" />
             {children}
           </ThemeProvider>
         </ConvexClientProvider>
